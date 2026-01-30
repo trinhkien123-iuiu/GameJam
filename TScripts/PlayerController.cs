fe8dfbbea07e28de6a1f6630404c395941f9df97
Thien <friend14112005@gmail.com>
2026-01-30 22:32:16 +0700

    Move Aiming and Shoot

---
+++ TScripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed = 5f;
+    Vector2 moveDir;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        moveDir = new Vector2(h, v).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = moveDir * speed;
+    }
+}
