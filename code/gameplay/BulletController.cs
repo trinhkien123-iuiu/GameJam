bca195223143cdc522ffa2f70c279c44db929837
Thien <friend14112005@gmail.com>
2026-01-31 00:54:52 +0700

    Player Shoot

---
+++ code/gameplay/BulletController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    Camera mainCam;
+    Vector3 direction;
+    public float speed = 10f;
+    public float diff = 0f;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+
+        Vector3 mouse = Input.mousePosition;
+        mouse.z = -mainCam.transform.position.z;
+        Vector3 mousePos = mainCam.ScreenToWorldPoint(mouse);
+
+        direction = (mousePos - transform.position).normalized;
+
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ + diff);
+    }
+
+    void Update()
+    {
+        transform.position += direction * speed * Time.deltaTime;
+    }
+}
